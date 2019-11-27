9d84221c1d341408a64e51e1a1fb37ae2a70b4a4
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2019-11-28 02:26:36 +0530

    Add files via upload

--- 7.c
+++ 7.c
@@ -1,70 +1,115 @@
 #include<stdio.h>
-#include<stdio.h>
-int complete(int com[],int r){
-	int i,flag=0;
+#include<stdlib.h>
+int p,r,i,av[10],j,max[10][10],alloc[10][10],need[10][10],comp[10]={0},req[10];
+void bankersalgo(){
+	int c1=0,c2=0,k;
+	while(c1!=p){
+		c2=c1;
+		for(i=0;i<p;i++){
+			k=0;
+			for(j=0;j<r;j++)
+				if(av[j]>=need[i][j])
+					k++;
+			if(k==r && comp[i]==0){
+				comp[i]=1;
+				c1++;
+				for(j=0;j<r;j++)
+					av[j]+=alloc[i][j];
+				printf("\nProcess %d",i);
+			}
+		}
 
-	for(i=0;i<r;i++)
-		if(com[i]==1)
-			flag=1;
-
-	return flag;
+		if(c1==c2){
+			printf("\nDeadlock\n");
+			return ;
+		}
+	}
 }
-
 
 void main(){
-	int i,j,k=0,p,r,com[10]={0},av[10],max[10][10],allot[10][10],nd[10][10],n;
-	printf("\nEnter the no of processess:");
+	int ch,pr_num,k;
+	printf("\nEnter the total no of proccess:");
 	scanf("%d",&p);
-	printf("\nEnter the no of different resources:");
+
+	printf("\nEnter the no of differnt types of resources:");
 	scanf("%d",&r);
 
+
+	printf("\nEnter the available instances of resource");
 	for(i=0;i<r;i++){
-		printf("\nEnter the currently available instances of resource type %d:",i+1);
+		printf("\nof type %d:",i+1);
 		scanf("%d",&av[i]);
-	}
+		}
 
 
+	printf("\nEnter the max no of resources required by  ");
 	for(i=0;i<p;i++){
-		printf("\nEnter the Max Value of resource req by process %d",i);
+		printf("\nProccess %d in order:",i+1);
+		for(j=0;j<r;j++)
+			scanf("%d",&max[i][j]);
+		}
+
+
+	printf("\nEnter the allocated no of resources to ");
+	for(i=0;i<p;i++){
+		printf("\nProccess %d in order:",i+1);
 		for(j=0;j<r;j++){
-		printf("\nResource of type %d:",j+1);
-		scanf("%d",&max[i][j]);
-		}
-	}
-
-
-	for(i=0;i<p;i++){
-                printf("\nEnter the Alloted  Value of resource to process %d",i);
-                for(j=0;j<r;j++){
-                printf("\nResource of type %d:",j+1);
-                scanf("%d",&allot[i][j]);
-		nd[i][j]=max[i][j]-allot[i][j];
-                }
-        }
-
-	n=p-1;
-
-	while(n--){
-		for(i=0;i<p;i++){
-			for(j=0;j<r;j++)
-				if(nd[i][j]<=av[j])
-					k++;
-		if(k==r && com[i]==0){
-			printf("Process - %d\n",i);
-			com[i]=1;
-			for(j=0;j<r;j++)
-				av[j]+=allot[i][j];
-		}
-		k=0;
-		}
-
-		if(n==p-2)
-			if(complete(com,r)==0){
-				printf("\nDeadlock\n");
-				break;
+			scanf("%d",&alloc[i][j]);
+			need[i][j]=max[i][j]-alloc[i][j];
 			}
+	       }
+
+
+	printf("\nNeed Matrix is as follows:");
+	for(i=0;i<p;i++){
+		for(j=0;j<r;j++)
+		       	printf("%d\t",need[i][j]);
+		printf("\n");
+		}
+
+	bankersalgo();
+
+	printf("\nRESOURCE REQUEST ALGORITHM\n");
+
+	printf("\nEnter the process number:");
+	scanf("%d",&pr_num);
+
+	printf("\nEnter the resource request  vector:");
+	for(i=0;i<r;i++)
+	       scanf("%d",&req[i]);
+
+	printf("\nEnter the available instances of resource");//Once again since available vetctor has changed after call to bankersalgo()
+        for(i=0;i<r;i++){
+        	printf("\nof type %d:",i+1);
+                scanf("%d",&av[i]);
+                }
+
+	for(i=0;i<p;i++)
+		comp[i]=0;
+
+	k=0;
+	for(i=0;i<r;i++)
+		if(req[i]<=need[pr_num-1][i] && req[i]<=av[i])
+			k++;
+	if(k==r){
+		for(i=0;i<r;i++){
+			av[i]-=req[i];
+
+			alloc[pr_num-1][i]+=req[i];
+
+			need[pr_num-1][i]-=req[i];
+		}
+
+
+		bankersalgo();
 	}
+
+	else
+		printf("\nERROR:REQUEST VECTOR IS GREATER THAN NEED \n OR \nERROR:REQUEST CANNOT BE GRANTED IMMIDIATELY\n");
+
 
 
 }
+
+
 
